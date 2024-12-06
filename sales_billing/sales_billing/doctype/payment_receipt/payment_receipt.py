c446caddecd527f1a80549a60f6fb40edd0e6e8c
Saralrat <saralrats@ecosoft.co.th>
2024-12-06 17:53:35 +0700

    add field total

--- sales_billing/sales_billing/doctype/payment_receipt/payment_receipt.py
+++ sales_billing/sales_billing/doctype/payment_receipt/payment_receipt.py
@@ -4,6 +4,19 @@
 import frappe
 from frappe.model.document import Document
 
-
 class PaymentReceipt(Document):
-    pass
+    def before_save(self):
+        self.update_total_sales_invoice_amount()
+        self.update_total_paid_amount()
+
+    def update_total_sales_invoice_amount(self):
+        total_sales_invoice_amount = 0
+        for row in self.billing_references:
+            total_sales_invoice_amount += row.grand_total
+        self.total_sales_invoice_amount = total_sales_invoice_amount
+
+    def update_total_paid_amount(self):
+        total_paid_amount = 0
+        for row in self.payment_references:
+            total_paid_amount += row.paid_amount
+        self.total_paid_amount = total_paid_amount
